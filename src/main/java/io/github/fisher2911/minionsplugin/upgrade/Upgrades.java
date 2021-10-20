fbfbe0b8285d8a6c386dce6b05a9d26381b426dc
HeroBrineGoat <76707404+MasterOfTheFish@users.noreply.github.com>
2021-10-19 21:16:19 -0400

    Added Cloud Command Framework dependency

--- src/main/java/io/github/fisher2911/minionsplugin/upgrade/Upgrades.java
+++ src/main/java/io/github/fisher2911/minionsplugin/upgrade/Upgrades.java
@@ -2,6 +2,9 @@ package io.github.fisher2911.minionsplugin.upgrade;
 
 public class Upgrades {
 
+    private final SpeedUpgrade speedUpgrade;
 
-
+    public Upgrades(final SpeedUpgrade speedUpgrade) {
+        this.speedUpgrade = speedUpgrade;
+    }
 }
